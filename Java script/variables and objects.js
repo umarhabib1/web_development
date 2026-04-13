c36f97df561c788b4b38ddec0d088c5b7734b0ab
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-04-13 23:52:17 +0500

    "variables and objects"

---
+++ Java script/variables and objects.js
@@ -0,0 +1,37 @@
+// console.log("hello");
+
+// var a = 8;  // we sould us let insted of var
+// var b = 9;
+// console.log(a + b + 3);
+
+// let z = "hello";
+// let y = 1;
+// console.log(z, y);
+// console.log(typeof z, typeof y);
+
+// const o = 67;
+// console.log(o);
+
+// let x = "umar";
+// let c = 12;
+// let v = true;
+// let n = undefined;
+// let m = null  ;
+// let s =  3.55 ;
+
+// console.log(x,c,v,n,m,s);
+// console.log(typeof x,typeof c,typeof v,typeof n,typeof m,typeof s);
+
+//                objects
+
+
+let obj = {
+    name: "m.umar habib",
+    class: "9th",   
+    isPRO : true
+}
+
+console.log(obj);
+obj.salary = "500core"
+
+
