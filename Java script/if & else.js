22705f189d1a89836077fdf271334935fc5af93a
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-04-14 23:44:19 +0500

    "if & else"

---
+++ Java script/if & else.js
@@ -0,0 +1,36 @@
+// console.log("if & else");
+
+
+// let age = 15;
+// let grace = 5;
+// age +=grace;
+// console.log(age);
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age % grace);
+// console.log(age ** grace);
+// if((age+grace)>18){
+//     console.log("you can drive");
+// }
+// else{
+//     console.log("No!,you can't drive")
+// }
+
+// let age = 0;
+
+// if(age==18){
+//     console.log("you can drive");
+// }else if (age === 0) {
+//     console.log("Are you kiding")
+// }
+// else{
+//     console.log("No!,you can't drive")
+// }
+
+let a  = 5;
+let b = 8;
+
+let result = a>b ? (a-b) : (b-a);
+console.log(result)
