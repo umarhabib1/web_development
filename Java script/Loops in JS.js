64544d5a40323a47650993fb3df1ea40849037f1
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-04-15 23:29:30 +0500

    "Loops in JS"

---
+++ Java script/Loops in JS.js
@@ -0,0 +1,39 @@
+console.log("Loops")
+// let a = 1;
+// for(let i=0;i<100;i++){
+//     console.log(i);
+// }
+
+// let obja ={
+//     name : "umar",
+//     rollno : 23,
+//     age : 15,
+//     class : "9th"
+// }
+
+// // let key = Object.keys[obja]
+// let values = Object.values(obja);
+// console.log(values);
+
+// let s = obja[key];
+// console.log(s);
+
+// for(key in obja){
+//     console.log(key);
+// }
+
+// for (const c of "umar") {
+//     console.log(c);
+// }
+
+// let i = 5;
+// while (i<6) {
+//     console.log(i);
+//     i++;
+// }
+
+let i = 10;
+do {
+    console.log(i);
+    i++
+} while (i<6);
