2cfd54db3239b2c046e9e218339f12cf61db3de5
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-04-16 20:56:42 +0500

    "functions in JS"

---
+++ Java script/functions in JS.js
@@ -0,0 +1,20 @@
+// function nice(name) {
+//     console.log("Hey " + name +" your are nice")
+//     console.log("Hey " + name +" your are good")
+//     console.log("Hey " + name +"  your are nice")
+// }
+
+// nice("umar")
+// nice("Hamza")
+ 
+
+// // sum(5,6);
+// let result = sum(5,6);
+// let result2 = sum(5,6,1);
+// console.log("the sum is",result);
+// console.log("the sum is",result2);
+
+// let func1 = (x)=>{
+//     console.log(x);
+// }
+// func1(23);
