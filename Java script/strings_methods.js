f610711e8981b3470fe2fd37925266cf804f83d6
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-04-18 23:00:14 +0500

    "strings_methods"

---
+++ Java script/strings_methods.js
@@ -0,0 +1,20 @@
+// let a ="umar"
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// console.log(a[4]);
+
+let real_name ="umar";
+let friend = "hasan";
+// console.log("His name is " +real_name+ " and his friend name is " +friend);
+
+// console.log(`His name is ${real_name} and his friend name is ${friend}`)
+
+let nam = "Hamza"
+console.log(nam.toUpperCase());
+console.log(nam.toLowerCase());
+console.log(nam.length);
+console.log(nam.slice(0,3));
+console.log(nam.concat(real_name,"nangyal"))
+console.log(nam.replace("Ha","67"))
