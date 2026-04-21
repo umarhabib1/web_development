bfd650747ca23fa23aa58a677caa9ceba5a7e2d3
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-04-21 22:29:27 +0500

    "basic JS"

---
+++ Java script/basic JS.js
@@ -0,0 +1,102 @@
+// console.log("umar");
+
+// age="14";
+// name=" m.umarhabib";
+// x ="null";
+// y ="undefined";
+// console.log(name);
+// console.log(age);
+// console.log(x);
+// console.log(y);
+
+// name = "umar";
+/*Name = "hassan";
+console.log(name);
+console.log(Name);*/
+
+/*
+_name= "hhaaahahhha";
+$name="jkjkjk";
+console.log(_name)*/
+
+// let name = "umar";
+// console.log(name);
+
+// var age ="41";
+// var age ="45";
+// var age ="23";
+// console.log(age);
+
+// let age ="41";
+// let age ="45";
+// let age ="23";
+// console.log(age);
+
+
+// const age="23";
+// // age="4"
+// console.log(age);
+
+// {
+//     let a ="12";
+//     console.log(a);
+// }
+
+// let a ="32";
+// console.log(a);
+
+// let  p = 67;
+// let  s = "umar";
+
+// let follow = true;
+// let x;
+// let y = null;
+
+// let x= BigInt(12312123);
+// let y = Symbol("hello");
+
+// const students ={
+//     Name : "mumarhabib",
+//     rollno: 24,
+//     age: 14,
+//     class : 8,
+//     marks : 999,
+//     percentage : 100,
+//     isPass : true,
+//     position : "first",
+
+// // Name : "mhassanhabib",
+// //     rollno: 18,
+// //     age: 14,
+// //     class : 8,
+// //     percentage : 20,
+// //     isPass : false,
+// //     position : "36",
+// }
+// students.marks=students.marks % 100
+
+// console.log(students["age"]);
+// console.log(students.marks)
+
+
+
+// PROJECT 1
+const product= {
+    name : "parkerpen" ,
+    rating :7/10,
+    dealOfTheDay : true,
+    price : 270,
+    discount : 5,
+}
+console.log(product);
+
+// project 2
+const profile = {
+    Name : "mumarhabib",
+    follow : true,
+    posts : 156,
+    followers : 10000000,
+    following : 0,
+    bio : "Experiance : in mincraft, in HTML,in CSS , JAVA, JS"
+}
+console.log(typeof profile.Name);
