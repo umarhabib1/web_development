1f78174f73b26a3b95c4ca44425035c15a334cff
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-04-22 23:24:47 +0500

    "Oprators in JS"

---
+++ Java script/Oprators in JS.js
@@ -0,0 +1,59 @@
+// Arithmatic opration
+
+/*let a = 2;
+let b = 8;
+console.log("a =" ,a,"& b =",b);
+console.log("a+b =",a+b);
+console.log("a-b =",a-b)
+console.log("a*b =" ,a*b)
+console.log("a / b =" ,a/b)
+console.log("a%b =" ,a%b)
+console.log("a**b =" ,a**b)*/
+
+// Unary Oprators   ++ , --
+
+// let a = 5;
+// console.log(a++);   // post increment
+// console.log(a);  
+// console.log(++a) // pre increment
+// console.log(a--); // post decrment
+// console.log(a);
+// console.log(--a);  // pre increment
+
+// Assignment Oprators =  +=, -= ,*=, /= , %=, = etc
+
+// let a = 6;
+// let b = 4;
+
+// a *= 4; // a = a * 4  
+// console.log(a)
+
+//Comparison Oprations
+// let a = 5;
+// let b = 8;
+// console.log(a!=b)
+// console.log(a==b)
+// console.log(a>=b)
+// console.log(a<=b)
+// console.log(a<b)
+// console.log(a>b)
+
+// let a = 5;
+// let b = "5";  
+// console.log(a==b) // string ---> number
+// console.log(a===b) // string ---> number + typeof
+
+// Logical oprators
+// let a = 5;
+// let b = 6;
+// let condition1 = a<b;
+// let condition2 = a===5;
+// console.log(condition1 && condition2)
+
+// let x = 8;
+// let y = 2;
+// console.log(x==y || x<y);
+
+// let j = 7;
+// let k = 9;
+// console.log(!(j==1))
