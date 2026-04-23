386429c2f922215bb1879cfc4dbb8bf752fb6cc3
Muhammad Umar Habib <mumarhabib1@gmail.com>
2026-04-23 23:30:08 +0500

    "conditional statments in JS"

---
+++ Java script/conditional statments.js
@@ -0,0 +1,48 @@
+// conditional statments
+
+let age = 20;
+
+if(age >18){
+   console.log("your are adult");
+}
+else{
+    console.log("you are a boy");
+}
+
+let num = 7*8;
+
+if(num%2 === 0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
+console.log(num)
+
+
+let mode = "light";
+let color;
+
+if(mode === "dark"){
+    color = "black";
+}
+else{
+    color  ="white";
+}
+// console.log(color)
+
+let age = 40;
+
+if(age<18){
+    console.log("junior")
+} else if(age>=40){
+    console.log("senior")
+}else{
+    console.log("middle")
+}
+let mode = "dark";
+
+if(mode === "dark"){
+    console.log("dark")
+} else if(mode = "light");
+   
