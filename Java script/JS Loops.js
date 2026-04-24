58acaee05d3f4045e31becaeec1992f903b1626a
umarhabib1 <mumarhabib1@gmail.com>
2026-04-24 10:52:37 -0700

    "Loops in JS"

---
+++ Java script/JS Loops.js
@@ -0,0 +1,46 @@
+
+// For Loop
+
+// for(let i=0;i<16;i++){
+//     console.log("umar");
+// }
+
+// let sum = 0;
+// let n =10;
+// for(let i = 1;i<=n;i++){
+//     sum=sum + i;
+// }
+// console.log(sum)
+
+// While loop
+// let i =0;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+// do while loop
+// let i = 0;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=10);
+
+// For-of loop
+// let str="umar";
+// let size = 0;
+// for(let i of str){
+//     console.log(i);
+//     size++;
+// }
+// console.log("size =",size);
+
+// for-in loop
+// let student={
+//     class : 8,
+//     rollno : 24,
+//     position : 1
+// }
+// for(let key in student){
+//     console.log("key =" ,key,"value =",student[key]);
+// }
