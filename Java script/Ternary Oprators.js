fbbbdacb5aefec71ab1d051df932c9464e845d7a
umarhabib1 <mumarhabib1@gmail.com>
2026-04-26 07:28:57 -0700

    Ternary Oprators

--- Java script/Ternary Oprators.js
+++ Java script/Ternary Oprators.js
@@ -1,14 +1,14 @@
-//Ternary Oprators
-// let age = 19;
-// age>=18?console.log("you are adult"): console.log("you are not adult"); 
+// Ternary Oprators
+let age = 19;
+age>=18?console.log("you are adult"): console.log("you are not adult"); 
 
 
-// project 1
-// let a = prompt("enter a number");
+project 1
+let a = prompt("enter a number");
 
-// if(a%5 == 0 ){
-//     console.log(a,"is a multiple of 5");
-// }
-// else{
-//     console.log( a,"is not a multiple of 5");
-// }
+if(a%5 == 0 ){
+    console.log(a,"is a multiple of 5");
+}
+else{
+    console.log( a,"is not a multiple of 5");
+}
