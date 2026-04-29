d03602336a0382029eb39d41734d24f5ae823571
umarhabib1 <mumarhabib1@gmail.com>
2026-04-29 10:48:34 -0700

    "arrays fuction"

---
+++ Java script/arrays fuction.js
@@ -0,0 +1,34 @@
+// let something = ["someone" , "boy" , "man"];
+// //something.push("pizza" , "apple" , "dog");
+// // something.pop();
+// // let deleatsomething = something.pop();
+// // console.log(something);
+// // console.log(deleatsomething);
+
+// console.log(something.toString())
+
+
+// let heros =    ["apple" , "banana" , "men"];
+// let villians = ["burger" , "pizza" ,"fries"];
+// // let noOne = heros.concat(villians);
+// heros.unshift("Madra");
+// villians.shift();
+// console.log(heros);
+// console.log(villians);
+// // console.log(noOne);
+
+// let someOne = ["burger" , "pizza" ,"fries","apple" , "banana" , "men"];
+// // console.log(someOne);
+// // console.log(someOne.slice(0 ,4));
+// someOne.splice(0,2,"one", "Kill")
+// console.log(someOne)
+
+// let arr = [1,2,3,4,5,6,7];
+// arr.splice(2,2,33,44)
+// console.log(arr)
+
+let arr = ["microsoft","hp","Dell","itel" ,"Mojang"];
+// arr.shift();
+arr.splice(1,1,"Noki")
+arr.push("Aloo");
+console.log(arr)
