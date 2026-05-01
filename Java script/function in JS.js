51855e95c05301b5a6726d8f5ea2e0e1dcef0488
umarhabib1 <mumarhabib1@gmail.com>
2026-05-01 11:38:42 -0700

    "function in JS"

---
+++ Java script/function in JS.js
@@ -0,0 +1,37 @@
+function xyz(ms ,n){
+
+console.log(ms*n);
+//     console.log("how are you ")
+// console.log(" i am fine")
+
+}
+
+xyz(10,7);
+
+// function sum(x,y){
+// console.log(x+y);
+// }
+
+// sum(100,101);
+function sum (x,y){
+  let  s = x+y; 
+    return s ;
+}
+let val = sum(7,5);
+console.log(val);
+
+
+function sum(a,b){
+    return a+b;
+}
+
+const arowSum = (a,b)=>{
+    console.log(a+b)
+}
+
+function multiple (x,y) {
+    return x*y;
+}
+const arrowMultiple = (x,y)=>{
+    console.log(x*y);
+}
