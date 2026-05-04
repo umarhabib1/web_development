6f442a1c9ec510cd7e5d7f608c1bc0395e78d649
umarhabib1 <mumarhabib1@gmail.com>
2026-05-04 11:11:18 -0700

    "arrays fuctions"

---
+++ Java script/arrays fuctions.js
@@ -0,0 +1,35 @@
+// String array
+// let strArr = ["schoolFreeCIty","malakand","Hss"];
+
+// strArr.forEach((val, i) => {
+//     console.log(val.toUpperCase(), i);
+// });
+
+// Project 1
+
+// let arr =[80,67,90,89,95,59,22,56,99,98,67,18]
+// let marks =arr.filter((val)=>{
+// return val > 90 ? val:0;
+// })
+// console.log(marks);
+
+// // Project 2
+// let user =  prompt("enter a number");
+
+// let arr =[]
+
+// for(let i =1;i<=user;i++){
+//     arr[i-1]=i;
+// }
+// console.log(arr);
+
+// let sum = arr.reduce((res,curr)=>{
+// return res+curr;
+// })
+// console.log(sum);
+
+
+// let factorial = arr.reduce((res,curr)=>{
+// return res*curr;
+// })
+// console.log(factorial);
