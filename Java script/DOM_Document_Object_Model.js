28a388d69ce6191e0c275d681c68e6bb97523b39
umarhabib1 <mumarhabib1@gmail.com>
2026-05-08 11:47:27 -0700

    "DOM_Document_Object_Model"

---
+++ Java script/DOM_Document_Object_Model.js
@@ -0,0 +1,70 @@
+// console.dir(document.body);
+// console.log(document.body);
+// console.log(document.body.childNodes[1].innerText = "abcd");
+// let head = document.getElementById("a1");
+// console.dir(head);
+// let heads = document.getElementsByClassName("c1");
+// console.log(heads);
+// console.dir(heads);
+
+// let tag = document.getElementsByTagName("p");
+// console.dir(tag);
+// let p1 = document.querySelector("p");
+// console.dir(p1)
+// let pAll = document.querySelectorAll("p");
+// console.dir(pAll);
+
+// let div = document.querySelector("div");
+// console.dir(div);
+
+// let head = document.querySelector("h1");
+// console.dir(head);
+
+// project 1
+// let h = document.querySelector("h2");
+// console.dir(h.innerText + "habibi");
+
+// Project 2
+// let div1 = document.querySelectorAll(".box");
+// console.dir(div1);
+
+
+// let id = document.querySelector("div");
+// console.dir(id.getAttribute("class"));
+// let  id2 = document.querySelector("p");
+// console.dir(id2.getAttribute("id"));
+
+
+// let id23 = document.getElementsByClassName("class");
+// id23[0].setAttribute("class", "newclass");
+
+// let div = document.querySelector("div");
+// div.style.backgroundColor= "red";
+
+// let newButton = document.createElement("button");
+// newButton.innerText = "click";
+// // div.append(newButton);
+// div.after(newButton);
+
+// let div = document.querySelector("div");  // or any selector
+
+// let newheading = document.createElement("h1");
+// newheading.innerHTML = "<i> Heading </i>";
+// newheading.style.backgroundColor = "red";
+// div.before(newheading);
+
+
+// let div = document.querySelector("div");
+// div.remove();
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// project 3
+// let body = document.querySelector("body");
+
+// let newButton = document.createElement("button");
+// newButton.innerText="click me";
+// body.append(newButton);
+
+let para = document.querySelector("P");
+para.classList.add("newclass")
