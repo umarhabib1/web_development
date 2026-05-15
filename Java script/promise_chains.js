10d16b3af2eccab16100205dea7e1a0192ed00a9
umarhabib1 <mumarhabib1@gmail.com>
2026-05-15 11:32:18 -0700

    "promise chains in JS"

---
+++ Java script/promise_chains.js
@@ -0,0 +1,106 @@
+
+////////////////////////////////////// Promises //////////////////////////////////////////////
+
+// let promise = new Promise((resolve,reject) =>{
+//     console.log("hello");
+//     // resolve("u are welcome");
+//     reject("some erro occured sorry");
+// });
+
+
+// function getData(dataId,getNextData) {
+//     return  new Promise((resolve ,reject) => {
+//     setTimeout(() =>{
+//         console.log("data =" , dataId);
+//         resolve("succes")
+//         if (getNextData) {
+//             getNextData();
+//         }
+//     },5000)
+// })}
+
+// const getPromise = () =>{
+//     return new Promise((resolve,reject) =>{
+//     console.log("hello");
+//     // resolve("succes");
+//     reject("network error")
+// });
+// }
+
+// let promise = getPromise();
+// promise.then((res) =>{
+//     console.log("promise fulfiled",res)
+// });
+
+// promise.catch((err) =>{
+//     console.log("reject",err)
+// })
+
+////////////////////////////////  Promise chain  ////////////////////////////////////////
+
+// function asyncfunction1() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() =>{
+//             console.log("some data1");
+//             resolve("succes");
+//         },4000)
+//     })
+// }
+
+
+// function asyncfunction2() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() =>{
+//             console.log("some data2");
+//             resolve("succes");
+//         },4000)
+//     })
+// }
+
+// console.log("geting data 1");
+// let p1 = asyncfunction1();
+// p1.then((res)=>{
+//     console.log("geting data 2");
+// let p2 = asyncfunction2();
+// })
+// console.log("geting data 2");
+// let p2 = asyncfunction2();
+// p1.then((res)=>{
+//     console.log(res)
+// })
+
+
+
+
+// function getData(dataId,) {
+//     return new Promise((resolve,reject) =>{
+     
+//     setTimeout(() =>{
+//         resolve("succes");
+//         console.log("data =" , dataId);
+//     },2000)})
+// }
+
+// //     Promis chain   //////
+
+// getData(1)
+// .then(() =>{
+//     return getData(2);
+// })
+// .then(() =>{
+//     return getData(3);
+    
+// })
+// .then(() =>{
+//     console.log("succes");
+// })
+
+
+// //     Callback hell //////
+// getData(1,()=>{
+//     getData(2,() =>{
+//         getData(3,() =>{
+//             getData(4)
+//         })
+//     })
+// })
