70f2e43598422e100fe6e482e966ec87141f6515
umarhabib1 <mumarhabib1@gmail.com>
2026-05-17 11:41:50 -0700

    "Call back hell in JS"

---
+++ Java script/Call_back_hell.js
@@ -0,0 +1,19 @@
+//////////////////////////// call back hell  ///////////////////////////////////////
+function getData(dataId,getNextData) {
+    setTimeout(() =>{
+        console.log("data =" , dataId);
+        if (getNextData) {
+            getNextData();
+        }
+    },2000)
+}
+
+
+// Callback hell
+getData(1,()=>{
+    getData(2,() =>{
+        getData(3,() =>{
+            getData(4)
+        })
+    })
+})
